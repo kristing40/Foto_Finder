0ffaf9637a4680563c5b8a7eeeb9feb3844ed488
Kristi Green <kngreen40@gmail.com>
2017-03-28 21:30:23 -0600

    Add puppy photo Modify js to add functionality to choose file button and add photo to album button Work in progress

--- SCRIPTS/scripts.js
+++ SCRIPTS/scripts.js
@@ -1,4 +1,35 @@
+/****Global Variables****/
+var $addToAlbumBtn = $('.add-to-album-btn');
 
+
+/****Event Listeners****/
+$addToAlbumBtn.on('click', function() {
+	console.log(this);
+	var $inputTitle = $('.input-title').val();
+	console.log($inputTitle)
+	var $inputCaption = $('.input-caption').val();
+	console.log($inputCaption)
+	var $inputFile = $('.inputfile').val();
+	console.log($inputFile)
+
+	photoCard($inputTitle, $inputCaption);
+});
+
+
+/****Functions****/
+function photoCard () {
+	$('#photo-display-area').append(`<article class="photo-card">
+			<p class="photo-card-title">${'$inputTitle'}<p>
+			<img class="photo-card-img" src=""/>
+			<p class="photo-card-caption">${'$inputCaption'}</p>
+			<div class="photo-card-bottom">
+				<input type="image" src="photos/delete.svg" class="photo-card-delete-btn" />
+				<input type="image" src="photos/favorite.svg" class="photo-card-fav-btn" />
+			</div>
+			</article>`);
+}
+
+/****Input File Button****/
 var inputs = document.querySelectorAll( '.inputfile' );
 Array.prototype.forEach.call( inputs, function( input )
 {
@@ -14,7 +45,7 @@ Array.prototype.forEach.call( inputs, function( input )
 			fileName = e.target.value.split( '\\' ).pop();
 
 		if( fileName )
-			label.querySelector( 'span' ).innerHTML = fileName;
+			label.querySelector('span').innerHTML = fileName;
 		else
 			label.innerHTML = labelVal;
 	});
